06679edab8e6ebd6ac6fdfe3357e9c29c453bacc
Joan Massich <sik@visor.udg.edu>
2014-12-07 20:39:20 +0100

    the keyword @abstractmethod it can only appear once.

---
+++ test.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+from sklearn.decomposition import PCA
+from abc import ABCMeta, abstractmethod
+import math
+
+
+class DataSimulationModel(object):
+    """ This is the real data D class for the case of simulation.
+        The simulated data can be drawn from different models which are
+        intended to inherit from this this class. Each model should :
+          -understand its own building parameters or through an error.
+          -implement the abstractmethods
+    """
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def __init__(self, *args, **kwargs):
+        print "this is the abstract class constructor"
+
+    def __str__(self):
+        pass
+
+    def generate_data(self, numSamples):
+        pass
+
+    def draw_model(self, axisId):
+        pass
+
+
+class MultiVariatedGaussianModel(DataSimulationModel):
+    def __init__(self, *args, **kwargs):
+        # args -- tuple of anonymous arguments
+        # kwargs -- dictionary of named arguments
+        print "this is the Child class constructor"
+        print 'args: ', args, ' kwargs: ', kwargs
+
+    def generate_data(sef, numSamples):
+        pass
+
+    def draw_model(self, axisId):
+        pass
