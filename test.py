6a05568a09ec8156a773b3a2c261096d89f18f23
Joan Massich <sik@visor.udg.edu>
2014-12-07 23:58:44 +0100

    Adding a dummy Datasimulation class, still the constructor dont call properly

--- test.py
+++ test.py
@@ -51,11 +51,11 @@ class MultiVariatedGaussianModel(DataSimulationModel):
         other argument would create an exception.
 
         Example:
-            MultiVariatedGaussianModel( modelMuX=0,
-                                        modelMuY=0,
-                                        modelSigmaX=1,
-                                        modelSigmaY=1,
-                                        modelSigmaXY=0,
+            MultiVariatedGaussianModel( modelMuX     = 0,
+                                        modelMuY     = 0,
+                                        modelSigmaX  = 1,
+                                        modelSigmaY  = 1,
+                                        modelSigmaXY = 0,
                                         )
     """
         # args -- tuple of anonymous arguments
@@ -72,10 +72,10 @@ class MultiVariatedGaussianModel(DataSimulationModel):
             self.modelSigmaXY = kwargs.pop(modelSigmaXY, 0)
 
     def __str__(self):
-        return 'Real-Data simulation (2D multi-variated Model):\n' \
+        return 'Real-Data simulation (2D multi-variate Model):\n' \
                '\tmean x:{0}, y:{1}\n' \
-               '\tstd  x:{2}, y:{3}, xy:{4}' \
-               '\n'.format(self.modelMuX, self.modelMuY, self.modelSigmaX, 
+               '\tSTD  x:{2}, y:{3}, xy:{4}' \
+               '\n'.format(self.modelMuX, self.modelMuY, self.modelSigmaX,
                       self.modelSigmaY, self.modelSigmaXY)
 
     def generate_data(sef, numSamples):
@@ -83,3 +83,30 @@ class MultiVariatedGaussianModel(DataSimulationModel):
 
     def draw_model(self, axisId):
         pass
+
+
+class DataSimulation(object):
+
+    """Docstring for DataSimulation. """
+
+    def __init__(self, randomSeed=140589, numSamplesPerModel=100):
+        """TODO: to be defined1.
+
+        :randomSeed: TODO
+        :models: TODO
+        :numSamplesPerModel: TODO
+
+        """
+        self._randomSeed = 15031984
+        self._models = zip(['blue, red'],
+                           [MultiVariatedGaussianModel(0, 1, 1.2, 1, 0.8),
+                            MultiVariatedGaussianModel(0, 0, 1.3, 0.7, 0.3)])
+        self._numSamplesPerModel = numSamplesPerModel
+
+    def _str__(self):
+        xx = ' '
+        for color, modelCharacteristics in self._models:
+            xx.join('{0} \n in colour {1} with {2} samples'.format(
+                     modelCharacteristics.__str__, color,
+                     self._numSamplesPerModel))
+        return xx
