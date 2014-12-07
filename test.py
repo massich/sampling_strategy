f3a2f741278b778f9fdf5f73944d3c6449fdb2eb
Joan Massich <sik@visor.udg.edu>
2014-12-07 23:12:38 +0100

    Added my first abstract constructor WTF!!!!

--- test.py
+++ test.py
@@ -17,6 +17,10 @@ class DataSimulationModel(object):
 
     @abstractmethod
     def __init__(self, *args, **kwargs):
+        # information in pytonic constructors:
+        #  http://tinyurl.com/3758j8u
+        #  http://tinyurl.com/424tbt7
+        #  http://tinyurl.com/lrownat
         pass
 
     def __str__(self):
@@ -30,10 +34,49 @@ class DataSimulationModel(object):
 
 
 class MultiVariatedGaussianModel(DataSimulationModel):
-    def __init__(self, *args, **kwargs):
+    """ This is the real data D class for the case of a single 2D Gaussian
+    multivariate simulation process.
+
+    Attributes:
+        modelMuX     -- x mean value of the model     (modelMuX     = 0)
+        modelMuY     -- y mean value of the model     (modelMuY     = 0)
+        modelSigmaX  -- variance of the model along x (modelSigmaX  = 1)
+        modelSigmaY  -- variance of the model along y (modelSigmaY  = 1)
+        modelSigmaXY -- x,y covariance of the model   (modelSigmaXY = 0)
+    """
+    def __init__(self, modelMuX, modelMuY, modelSigmaX, modelSigmaY,
+                 modelSigmaXY, *args, **kwargs):
+        """ The following parameters can be specified in order to define define
+        a multivariate 2D Gaussian model. Otherwise, defaults my apply. Any
+        other argument would create an exception.
+
+        Example:
+            MultiVariatedGaussianModel( modelMuX=0,
+                                        modelMuY=0,
+                                        modelSigmaX=1,
+                                        modelSigmaY=1,
+                                        modelSigmaXY=0,
+                                        )
+    """
         # args -- tuple of anonymous arguments
         # kwargs -- dictionary of named arguments
-        print 'args: ', args, ' kwargs: ', kwargs
+        diff = kwargs.viewkeys() - {'modelSigmaXY', 'modelSigmaX',
+                'modelSigmaY', 'modelMuY', 'modelMuX'}
+        if diff:
+            raise Exception("Invalid args: " + ', '.join(diff))
+        else:
+            self.modelMuX     = kwargs.pop(modelMuX,     0)
+            self.modelMuY     = kwargs.pop(modelMuY,     0)
+            self.modelSigmaX  = kwargs.pop(modelSigmaX,  1)
+            self.modelSigmaY  = kwargs.pop(modelSigmaY,  1)
+            self.modelSigmaXY = kwargs.pop(modelSigmaXY, 0)
+
+    def __str__(self):
+        return 'Real-Data simulation (2D multi-variated Model):\n' \
+               '\tmean x:{0}, y:{1}\n' \
+               '\tstd  x:{2}, y:{3}, xy:{4}' \
+               '\n'.format(self.modelMuX, self.modelMuY, self.modelSigmaX, 
+                      self.modelSigmaY, self.modelSigmaXY)
 
     def generate_data(sef, numSamples):
         pass
