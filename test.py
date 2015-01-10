a7548b4bb40482031d7b6046c0844acee97d3928
Joan Massich <sik@visor.udg.edu>
2015-01-10 22:07:01 +0100

    Added test.py as the contoroler of the experiment

--- test.py
+++ test.py
@@ -10,21 +10,20 @@ from random_sampling import *
 import matplotlib.pyplot as plt
 
 x = DataSimulation(randomSeed=15031984)
-#yy = x._models.pop('blue')
-#print yy
 
-#print yy.data_model_information()
-print x
+# Show the data
+fig = plt.figure()
+ax1 = fig.add_subplot(221)
+ax1.axis(x.data_range())
+x.show_all_data(ax1)
 
-# print 'From the Datasimulation Default'
-# x = DataSimulation()
-# print x._models[0]
-# print x._models[1]
-#
-# print 'From a created string'
-# xx = ' '
-# for puta in self._models:
-#     xx.join('{0} \n in colour {1} with {2} samples'.format(
-#              puta.__str__, 'red',
-#              self._numSamplesPerModel))
-# print xx
+# Calculate the Data projection
+r = RandomSampler(x)
+r.show_projection(ax1)
+ax2 = fig.add_subplot(222)
+r.show_projection_axes(ax2)
+r.show_projected_samples(ax2)
+
+# Show selected samples 
+ax3 = fig.add_subplot(223)
+r.show_selected_samples(ax3)
