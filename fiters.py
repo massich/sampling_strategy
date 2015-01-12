17b305e490e163873f3424eb3250f24729b3ef02
Joan Massich <sik@visor.udg.edu>
2015-01-12 16:33:36 +0100

    Changed fiters to projection Models

--- fiters.py
+++ fiters.py
@@ -1,32 +1,31 @@
-
 import numpy as np
 from sklearn.decomposition import PCA
 
-class FiterSingleFeat(object):
 
-    """Docstring for FiterSingleFeat. """
+class ProjectionModelSingleFeat(object):
 
-    def __init__(self):
-        """TODO: to be defined1. """
-
-
-class FiterLDA(object):
-
-    """Docstring for FiterLDA. """
+    """Docstring for ProjectionModelSingleFeat. """
 
     def __init__(self):
         """TODO: to be defined1. """
 
 
-class FiterPCA(object):
+class ProjectionModelLDA(object):
+
+    """Docstring for ProjectionModelLDA. """
+
+    def __init__(self):
+        """TODO: to be defined1. """
+
+
+class ProjectionModelPCA(object):
 
     """Docstring for fiterPCL. """
 
     def __init__(self, data):
         """TODO: to be defined1. """
         num_dims_to_keep = 2
-        self._transformation = PCA(n_components = num_dims_to_keep).fit(data)
-
+        self._transformation = PCA(n_components=num_dims_to_keep).fit(data)
 
     def project_data(self, data):
         return self._transformation.transform(data)
@@ -37,41 +36,38 @@ class FiterPCA(object):
         :axisId: axis to plot on
 
         """
-        base = np.array([[-1,1,0,0], [0,0,-1,1]]).T
+        base = np.array([[-1, 1, 0, 0], [0, 0, -1, 1]]).T
         base_projected = self._transformation.transform(6*base)
 
         print('projected base')
         print(base_projected)
 
-        x,y = base_projected.T
+        x, y = base_projected.T
         axisId.plot(x[0:2], y[0:2], 'k-', linewidth=2)
         axisId.plot(x[2:4], y[2:4], 'k-', linewidth=2)
 
-
-import matplotlib.pyplot as plt
 
 def main():
     """
         Tests
     """
 
-    data = np.array([[1,2,3,4,5], [1,2,3,4,5]]).T
+    import matplotlib.pyplot as plt
+
+    data = np.array([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]).T
     print(data.shape)
     print(data)
 
-    my_fiter = FiterPCA(data)
-    data_projected = my_fiter.project_data(data)
+    my_projMod = ProjectionModelPCA(data)
+    data_projected = my_projMod.project_data(data)
 
     print('projected data:')
     print(data_projected)
 
-
     fig = plt.figure()
     axis = fig.add_subplot(111)
-    my_fiter.display_base(axis)
+    my_projMod.display_base(axis)
     fig.show()
 
 if __name__ == '__main__':
     main()
-
-
