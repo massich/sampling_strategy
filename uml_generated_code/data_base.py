158f0994bf6e49fef36c67fbf1ababd6c35ae55e
Joan Massich <sik@visor.udg.edu>
2015-01-17 10:48:16 +0100

    starting data_base.py

--- uml_generated_code/data_base.py
+++ uml_generated_code/data_base.py
@@ -8,12 +8,27 @@ from IProjectionModel import *
 
 
 class DataBase(object):
-    """ DataBase stores the data as 3 dictionaries that can be indexed using
-        the class name
+    """DataBase is a (class, model, samples) triplet diccionary that can be
+    accessed using the class name of the data.
+    
+    Note:
+      * the names of the data class **must** be unique, since they are used the
+      dictionary key.
 
-    :version:
+    Attributes:
+      self (dictionary of (DataClassInstance, IDataModel, npArray)): each
+        element corresponds to a different group of data within the data base.
+
+    :version: 0.0.1
     :author: sik
     """
+# >>> t = [(4, 180, 21), (5, 90, 10), (3, 270, 8), (4, 0, 7)]
+# >>> dict([ (k, [v, w]) for k, v, w in t ])
+# {3: [270, 8], 4: [0, 7], 5: [90, 10]}
+#
+
+    def __new__(cls):
+        return dict()
 
     def get_class_names(self):
         """
