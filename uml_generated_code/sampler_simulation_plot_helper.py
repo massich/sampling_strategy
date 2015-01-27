9135886536700b32e812813e24f3cfd51e661f67
Joan Massich <sik@visor.udg.edu>
2015-01-27 19:39:05 +0100

    Up to DataBase plotting

--- uml_generated_code/sampler_simulation_plot_helper.py
+++ uml_generated_code/sampler_simulation_plot_helper.py
@@ -1,4 +1,7 @@
-"""random thoughts - sampling_strategy, plotting support code"""
+"""random thoughts - sampling_strategy, plotting support code
+
+   .. todo:: [code] merge the functions and use multifunction decorator
+"""
 
 # from multipledispatch import dispatch
 from data_base import *
@@ -39,7 +42,56 @@ def scatterPlot_realData_nparray_in_dbSpace(ax, data, param_dict=dict()):
     :version: 0.0.1
     :author: sik
     """
-    return ax.scatter(data[:, 0], data[:, 1], **param_dict)
+    sampleDrawingOptions = {'linewidths': 0.1, 'alpha': 0.7}
+    sampleDrawingOptions.update(param_dict)
+    return ax.scatter(data[:, 0], data[:, 1], **sampleDrawingOptions)
+
+
+def plot_DBElement_in_dbSpace(ax, data, param_dict={}):
+    """ Display a DBElement accordignly to its characteristics
+
+    Args:
+        ax (axes): The axes to draw to.
+        data (DBElement): element to be displayed in *ax*
+
+    Returns:
+        A list of artists added to **ax** while plotting
+
+    :rtype: list
+    :version: 0.0.1
+    :author: sik
+    """
+    # Set-up the DBElement's characteristics into param_dict
+    sampleDrawingOptions, modelDrawingOptions = dict(param_dict), dict(param_dict)
+    sampleDrawingOptions.update({'c': data.dbeClass.color})
+    modelDrawingOptions.update({'colors': data.dbeClass.color})
+
+    samplesOut = scatterPlot_realData_nparray_in_dbSpace(
+        ax, data.dbeSamples, sampleDrawingOptions)
+    modelOut = plot_realData_model_isolines_in_dbSpace(
+        ax, data.dbeModel, modelDrawingOptions)
+
+    return samplesOut, modelOut
+
+def plot_DataBase_in_dbSpace(ax, data, param_dict={}):
+    """ Display a DBElement accordignly to its characteristics
+
+    Args:
+        ax (axes): The axes to draw to.
+        data (DataBase): entire DataBase to be displayed in *ax*
+
+    Returns:
+        A list of artists added to **ax** while plotting
+
+    .. todo:: [code] the return is not implemetned
+
+    :rtype: list
+    :version: 0.0.1
+    :author: sik
+    """
+    for d in data.itervalues():
+        print d
+        plot_DBElement_in_dbSpace(ax, d, **param_dict)
 
 
 def _test():
@@ -56,12 +108,20 @@ def _test():
     xx2 = myDataBaseExample['red'].dbeSamples
     mm2 = myDataBaseExample['red'].dbeModel
 
-    fig, ax = plt.subplots()
-    scatterPlot_realData_nparray_in_dbSpace(ax, xx1, {'marker': 'x'})
-    scatterPlot_realData_nparray_in_dbSpace(ax, xx2)
+    fig, (ax1, ax2, ax3)  = plt.subplots(ncols=3)
+    for aa in [ax1, ax2, ax3]:
+        aa.axis([-3, 3, -3, 3])
 
-    plot_realData_model_isolines_in_dbSpace(ax, mm1)
-    plot_realData_model_isolines_in_dbSpace(ax, mm2, {'colors': 'g'})
+    scatterPlot_realData_nparray_in_dbSpace(ax1, xx1, {'marker': 'x'})
+    scatterPlot_realData_nparray_in_dbSpace(ax1, xx2)
+
+    plot_realData_model_isolines_in_dbSpace(ax1, mm1)
+    plot_realData_model_isolines_in_dbSpace(ax1, mm2, {'colors': 'g'})
+
+    plot_DBElement_in_dbSpace(ax2, myDataBaseExample['blue'])
+    plot_DBElement_in_dbSpace(ax2, myDataBaseExample['red'])
+
+    plot_DataBase_in_dbSpace(ax3, myDataBaseExample)
 
     plt.show()
 
